eb18100d218d80c27693785866e6585aa840100c
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 17:33:28 +0200

    Check if the sent string is of type string

--- palindrome.php
+++ palindrome.php
@@ -1,4 +1,15 @@
 <?php 
 
+if(!isset($_GET['string'])){
+    http_response_code(400);
+    echo json_encode(['error'=>'String is not sent']);
+    exit;
+}
+$string = $_GET['string'];
+if(!is_string($string)){
+    http_response_code(400);
+    echo json_encode(['error'=>'string param is not of type string']);
+    exit;
+}
 
 ?>
