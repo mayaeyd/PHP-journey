8d6e98f3238ee7b6c65838443c022f828732a65c
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 18:34:57 +0200

    Assign variables for left, middle and right parts of array

--- merge_sort.php
+++ merge_sort.php
@@ -3,8 +3,21 @@
 if(!isset($_GET['array'])){
     http_response_code(400);
     echo json_encode(['error'=>'Array is not set in the params']);
+    exit;
 }
 
+$array = $_GET['array'];
+if(!is_array($array)){
+    http_response_code(400);
+    echo json_encode(['error'=>'Array sent is not of type array']);
+    exit;
+}
 
+function merge_sort($array){
+    $middle = floor(count($array)/2);
+
+    $left = array_slice($array, 0, $middle);
+    $right = array_slice($array,$middle);
+}
 
 ?>
