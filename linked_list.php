d6b3ba6298c68cd850358ca46ac855b82a4be95b
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 20:26:42 +0200

    Initilaize node method

---
+++ linked_list.php
@@ -0,0 +1,17 @@
+class Node{
+ public $data;
+ public $next;
+
+ function set_data($data) {
+    $this->data = $data;
+  }
+ function get_name() {
+    return $this->data;
+  }
+
+}
+
+class LinkedList{
+    private $head;
+
+}
