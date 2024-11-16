77f8e3cf72f90c35e7857bc52c012091b876621f
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 17:49:28 +0200

    Clean the code

--- palindrome.php
+++ palindrome.php
@@ -14,16 +14,23 @@ if(!is_string($string)){
 
 $clean_string = '';
 for($i=0;$i<strlen($clean_string); $i++){
-    if(($sring[$i]>='A' && $string[$i]<='Z') || ($sring[$i]>='a' && $string[$i]<='z')){
-        $clean_string .= strtolower($string[$i]);
+    $char=$string[i];
+    if(($char>='A' && $char<='Z') || ($char>='a' && $char<='z')){
+        $clean_string .= strtolower($char);
     }
 }
 
 //check if palindrome
+$is_palindrome=true;
 for($i=0; $i<strlen($clean_string)/2; $i++){
     if($clean_string[i]!== $clean_string[strlen($clean_string)-i-1]){
-        echo json_encode(['error'=>'String is not palindrome']);
+        $is_palindrome =false;
+        echo json_encode(['is Palindrome'=>$is_palindrome]);
+        exit;
     }
 }
+
+http_response_code(200);
+echo json_encode(['is Palindrome'=>$is_palindrome]);
 
 ?>
