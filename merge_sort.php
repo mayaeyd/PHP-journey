34279da72781858f37d1dd7271c4062839698802
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 19:37:02 +0200

    Add least number to result array

--- merge_sort.php
+++ merge_sort.php
@@ -6,7 +6,8 @@ if(!isset($_GET['array'])){
     exit;
 }
 
-$array = $_GET['array'];
+$array = json_decode($_GET['array'], true);
+
 if(!is_array($array)){
     http_response_code(400);
     echo json_encode(['error'=>'Array sent is not of type array']);
@@ -18,13 +19,32 @@ function merge_sort($array){
         return $array;
     }
 
-    $middle = floor(count($array)/2);
+    $middle = floor(count($array)/2);//2
 
-    $left = array_slice($array, 0, $middle);
-    $right = array_slice($array,$middle);
+    //2,5,3,1,6
+    $left = array_slice($array, 0, $middle); //2,5
+    $right = array_slice($array,$middle);//3,1,6
 
     $left = merge_sort($left);
     $right = merge_sort($right);
+
+    return merge($left, $right);
+}
+
+function merge($left, $right) {
+    $result = [];
+    $i = 0;
+    $j = 0;
+
+    while($i<count($left) && $j<count($right)){
+        if($left[$i]<=$right[$j]){
+            $result[] = $left[$i];
+            $i++;
+        }else{
+            $result[]=$right[$j];
+            $j++;
+        }
+    }
 }
 
 ?>
