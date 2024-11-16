a312cde9e82fc628f8aec45a4298359114209cb5
Maya Eid <mayabaseleid@gmail.com>
2024-11-16 21:12:46 +0200

    Check if count reached 2 and return the node

--- linked_list.php
+++ linked_list.php
@@ -1,3 +1,5 @@
+<?php
+
 class Node{
     public $data = NULL;
     public $next = NULL;
@@ -13,31 +15,44 @@ class LinkedList{
 
     public function insert($data){
         $new_node = new Node($data);
-        if($this->head = NULL){
+        if($this->head === NULL){
             $this->head = $new_node;
         }else{
             $current_node = $this->head;
             while($current_node->next !== NULL){
                 $current_node = $current_node->next;
             }
-            $current_node= $new_node;
+            $current_node->next= $new_node;
         }
     }
 
     public function traverse(){
         $vowel_count = 0;
+        $result = [];
         $current_node = $this->head;
-        while($current_node->next !== NULL){
+
+        while($current_node !== NULL){
+
+            //check if string
             if(is_string($current_node->data)){
-                $string=strtolower($current_node->data)
+                $string=strtolower($current_node->data);
                 for($i=0; $i<strlen($string); $i++){
                     $char = $string[$i];
-                    if($char = 'a' || $char = 'e' || $char = 'i' || $char = 'o' || $char = 'u'){
+
+                    //count vowels
+                    if($char == 'a' || $char == 'e' || $char == 'i' || $char == 'o' || $char == 'u'){
                         $vowel_count++;
+                    }
+                    if($vowel_count==2){
+                        $result[] =$string;
+                        break;
                     }
                 }
             }
             $current_node = $current_node->next;
         }
+        return json_encode(['Nodes with two vowels '=>$result]);
     }
 }
+
+?>
